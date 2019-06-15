0e7fa52538ecb0a11e4427899fe1580668703006
Chiara Ripanti <chiararipanti@MacBook-Air-di-Chiara.local>
2019-06-15 12:49:32 +0100

    set up client routers and created separate components

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
+import React from 'react'
+import AppRouter from './routers/AppRouter'
 import 'normalize.css/normalize.css' 
 import './styles/styles.scss'
 
 //Componenti Stateless sono piu veloci perche non devono gestire lo stato
 //Non gestiscono il lifecycle del componente (fire some methods ecc..)
 
-ReactDOM.render(<p>This is the new app</p>, document.getElementById("app"))
+ReactDOM.render(<AppRouter />, document.getElementById("app"))
